016ab84be2e20ddb06f18249763bcf2d4ffe9bb6
Mike Turley <mike@miketurley.com>
2014-11-18 07:49:25 -0500

    Slide animations on the tab bar controller

---
+++ WMUA-Radio/WMUATabBarController.h
@@ -0,0 +1,13 @@
+//
+//  WMUATabBarController.h
+//  WMUA-Radio
+//
+//  Created by Mike Turley on 11/18/14.
+//  Copyright (c) 2014 Mike Turley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WMUATabBarController : UITabBarController <UITabBarControllerDelegate>
+
+@end
